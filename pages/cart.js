d0fecb1ccb74139e51c19296a902525481bf5345
Farhod <ubaydullaevfarhod18@gmail.com>
2023-08-16 17:35:26 +0500

    rebuild

--- pages/cart.js
+++ pages/cart.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import CartMain from '../components/CartMain';
-import { StaticDataSingleton } from '../utils/staticData';
-import { HeadData } from '../components/Head';
+import React from 'react'
+import Layout from '../components/Layout'
+import CartMain from '../components/CartMain'
+import { StaticDataSingleton } from '../utils/staticData'
+import { HeadData } from '../components/Head'
 
 const Cart = ({ categories }) => {
-  // console.log(categories)
   return (
     <>
-      <HeadData title={'Корзина'} pageUrl="/cart" />
+      <HeadData title={'Корзина'} pageUrl='/cart' />
       <Layout categories={categories}>
         <CartMain />
       </Layout>
     </>
-  );
-};
+  )
+}
 
 export async function getStaticProps() {
-  const staticData = new StaticDataSingleton();
-  await staticData.checkAndFetch(true);
+  const staticData = new StaticDataSingleton()
+  await staticData.checkAndFetch(true)
 
-  const categories = staticData.getRootCategories();
+  const categories = staticData.getRootCategories()
 
   return {
     props: {
       categories: categories.allCategories,
     },
     revalidate: 60,
-  };
+  }
 }
 
-export default Cart;
+export default Cart
